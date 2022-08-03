472c8c7d35a93115e9020d7b33d026cae3a4bb23
CTM <clemente1875gmail.com>
2022-08-03 22:31:37 +0200

    añadimos redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { JournalApp } from './JournalApp'
+import { store } from './store'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    {/* creamos el provider para usar redux y como parametro le pasamos el store.js creado*/}
+    <Provider store={ store }>
+      {/* creamos el BrowserRouter para usar las rutas de navegacion */}
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+
+//creamos el store para trabajar con redux
+
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {
+
+
+  },
+});
